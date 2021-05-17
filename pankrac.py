9a1afaf07dc50e140ed368bb22c750eafb7ff54f
Jan Zeman <janzeman3@gmail.com>
2021-05-17 10:10:40 +0200

    Zobecnení na 2 typy akcí text a metoda
    Signed-off-by: Jan Zeman <janzeman3@gmail.com>

--- pankrac.py
+++ pankrac.py
@@ -2,6 +2,9 @@ import json
 
 from pankracutils import obsahuje
 
+TYPE_TEXT = 1
+TYPE_METHOD = 2
+
 LINK_WEB_STEZKA = "https://stezka.skaut.cz/prohlizej-a-inspiruj-se/"
 LINK_NOTION_VYZVY = "https://www.notion.so/janzeman3/0995fe1d94a9403e99e667fc2ad15e30?v=3d42ab631c064ce0a16dda28bd06439d"
 LINK_NOTION_SPLNENE = "https://www.notion.so/janzeman3/3f6b1919e9bd49eaa46e2e21108ba0ce?v=62bb60897c594cf2ab1d8c30cab459d7"
@@ -13,52 +16,62 @@ class Pankrac:
     def __init__(self):
         uzel_spln = {'keys': ["spln"],
                          'subnodes': [],
-                         'action': self.splnena_stezka
+                         'action': {'type': TYPE_METHOD, 'data': self.splnena_stezka}
                          }
 
         uzel_stezka_na_webu = {'keys': ["stezk"],
                          'subnodes': [uzel_spln],
-                         'action': self.stezka_na_webu
+                         'action': {'type': TYPE_METHOD, 'data':  self.stezka_na_webu}
                          }
 
         uzel_novacek_na_webu = {'keys': ["nováč"],
                          'subnodes': [uzel_spln],
-                         'action': self.web_novacek
+                         'action': {'type': TYPE_METHOD, 'data':  self.web_novacek}
                          }
 
         uzel_vyzvy = {'keys': ["výzv"],
                          'subnodes': [uzel_spln],
-                         'action': self.web_vyzvy
+                         'action': {'type': TYPE_METHOD, 'data':  self.web_vyzvy}
                          }
 
         uzel_generuj_heslo = {'keys': ["heslo"],
                          'subnodes': [],
-                         'action': self.generuj_heslo
+                         'action': {'type': TYPE_METHOD, 'data':  self.generuj_heslo}
                          }
 
         uzel_akce = {'keys': ["akce"],
                          'subnodes': [],
-                         'action': self.web_akce
+                         'action': {'type': TYPE_METHOD, 'data':  self.web_akce}
                          }
 
         uzel_sokoli_web = {'keys': ["s sebou"],
                          'subnodes': [],
-                         'action': self.web_sokoli
+                         'action': {'type': TYPE_METHOD, 'data':  self.web_sokoli}
                          }
 
 
         uzel_help = {'keys': ["nápověd", "pomoc", "help", "příkazy", "/"],
                          'subnodes': [],
-                         'action': self.napoveda
+                         'action': {'type': TYPE_METHOD, 'data':  self.napoveda}
                          }
 
         self.moznosti = {'keys': ["Pankráci"],
                          'subnodes': [uzel_sokoli_web, uzel_vyzvy, uzel_stezka_na_webu, uzel_novacek_na_webu, uzel_generuj_heslo, uzel_akce, uzel_help],
-                         'action': self.nevim
+                         'action': {'type': TYPE_METHOD, 'data':  self.nevim}
                          }
 
     def nevim(self, message_text):
         return "Tady Pankrác, slyším Tě, ale ale nevím, co po mě chceš. Zkus napsat -Pankráci pomoc!-"
+
+    ## obdrží akci a vygeneruje její výsledek na základě dané otázky
+    def vysledek_akce(self, akce, otazka):
+        if akce['type'] == TYPE_METHOD:
+            odpoved = akce['data'](otazka)
+        elif akce['type'] == TYPE_TEXT:
+            odpoved = akce['data']
+        else:
+            odpoved = "Chyba dat kontaktuj programátory..."
+        return odpoved
 
     ## zpracuje odezvu podle obsahu proměnné self.moznosti
     def zpracuj_odezvu(self, message):
@@ -88,8 +101,7 @@ class Pankrac:
                 uzel = novy_uzel
 
         # nakonec provedu akci z finálního uzlu
-        akce = uzel['action']
-        return akce(otazka)
+        return self.vysledek_akce(uzel['action'], otazka)
 
     def generuj_hierarchii(self, uzel, odsazeni):
         hierarchie = "".ljust(odsazeni*4, " ") + "- "
